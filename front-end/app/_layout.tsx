e39eec7c4c51777380864a3819453e3469d7c2e0
Ayman Rahman <ayman2002rahman@gmail.com>
2025-04-12 18:24:19 -0500

    Initall install command

--- front-end/app/_layout.tsx
+++ front-end/app/_layout.tsx
@@ -43,7 +43,8 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name='index' options={{ headerShown: false }} />
-      <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+      <Stack.Screen name='disabilityQuestion' options={{ headerShown: false }} />
+      <Stack.Screen name='(tabs)' options={{ headerShown: false, gestureEnabled: false }} />
     </Stack>
   );
 }
--- front-end/app/disabilityQuestion.tsx
+++ front-end/app/disabilityQuestion.tsx
@@ -1,19 +1,24 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native'
 import { useRouter } from 'expo-router';
 
-export default function WelcomeScreen() {
+export default function DisabilityQuestionScreen() {
     const router = useRouter();
+
+    //const use
 
     return (
         <View className='flex-1'>
             <View className='flex-1 justify-center items-center'>
-                <Text>Welcome to our app!</Text>
+                <Text>Whats you're disability?</Text>
+                <TextInput 
+                    placeholder='Email'
+                />
             </View>
             <View className='absolute bottom-[65px] w-full'>
                 <View className='flex-1 justify-center items-center px-[50px]'>
                     <TouchableOpacity 
                         className='flex justify-center items-center w-full bg-black py-[20px] rounded-full'
-                        onPress={() => {router.push('/disabilityQuestion')}}
+                        onPress={() => {router.push('/(tabs)')}}
                     >
                         <Text className='text-white'>Get Started!</Text>
                     </TouchableOpacity>
