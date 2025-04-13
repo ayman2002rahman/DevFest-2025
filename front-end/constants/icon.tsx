e2ec142cd1e0886ab456f1beaf1b57300d563a26
Ayman Rahman <ayman2002rahman@gmail.com>
2025-04-13 12:49:21 -0500

    some map done

--- front-end/constants/icon.tsx
+++ front-end/constants/icon.tsx
@@ -1,6 +1,6 @@
 import { Ionicons } from "@expo/vector-icons";
 
 export const icon: Record<'index' | 'scavengerHunt', (props: any) => JSX.Element> = {
-    scavengerHunt: (props: any) => <Ionicons name='home' size={24} {...props} />,
-    index: (props: any) => <Ionicons name='home' size={24} {...props} />,
+    scavengerHunt: (props: any) => <Ionicons name='document-text-outline' size={24} {...props} />,
+    index: (props: any) => <Ionicons name='trophy-outline' size={24} {...props} />,
 };
